4e92252185c8ac864d61e4bc25e10c4772111c2f
Ayushi-Doshi <85425548+Ayushi-Doshi@users.noreply.github.com>
2022-03-28 20:20:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+mustachex = 0;
+mustachey = 0;
+function preload(){
+mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Intialized');
+}
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        mustachex = results[0].pose.nose.x-24;
+        mustachey = results[0].pose.nose.y+5;
+        console.log("mustache x = " + mustachex);
+        console.log("mustache y = " + mustachey);
+    }
+}
+
+function draw(){
+    image(video , 0 ,0 , 300, 300);
+   image(mustache, mustachex,mustachey,50,25);
+}
+
+function take_snapshot(){
+    save('myFilterIamge.png');
+}
